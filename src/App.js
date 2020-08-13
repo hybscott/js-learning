18b4e58cdba46d154b831e3d5b5a1f7b669583be
Scott.Huang <scott.huang@skylinetw.com>
2020-08-13 14:40:01 +0800

    設定版面與路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
+import "antd/dist/antd.css";
 import Routes from "./routes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Routes />
-      </header>
+      <Routes />
     </div>
   );
 }
--- src/pages/template/index.js
+++ src/pages/template/index.js
@@ -1,5 +1,54 @@
 import React from "react";
+import { Layout, Menu, Icon } from "antd";
+import "./index.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+const { Header, Sider, Content } = Layout;
+
 export default function Index({ component }) {
   const Name = component;
-  return <Name />;
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider trigger={null} collapsible collapsed={false}>
+        <div className="logo" style={{ height: "32px", margin: "16px" }} />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">
+            <Link to="/">
+              <Icon type="user" />
+              <span>catalog</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/admin">
+              <Icon type="user" />
+              <span>admin</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/home">
+              <Icon type="user" />
+              <span>home</span>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header style={{ background: "#fff", padding: 0 }}>
+          <Icon
+            className="trigger"
+            type={false ? "menu-unfold" : "menu-fold"}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            background: "#fff",
+            minHeight: 280
+          }}
+        >
+          <Name />
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
